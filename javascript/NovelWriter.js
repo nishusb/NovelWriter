0d9e0d1ce267b52a636d568f756b42c6409b78a4
vnms <viniciusbyrne@gmail.com>
2016-10-06 16:57:41 -1000

    Improvements

--- javascript/NovelWriter.js
+++ javascript/NovelWriter.js
@@ -33,23 +33,26 @@ function sceneUpdate(t, c, s) {
 }*/
 
 function addChap() {
-  if (Novel.chapters.length == 0) {
-    var nl0 = true;
-  }
-  chapter(Novel.chapters.length+1+": "+prompt("Chapter Name:"));
-  if (nl0) {
-    Novel.chapters[0].text = document.getElementById("edit").value;
-    cc(0);
-  }
+  chapter(Novel.chapters.length+1);
+  cc(Novel.chapters.length-1);
   updateChapSel();
 }
 
+function removeOutput() {
+  $("#output").hide(500);
+}
 function exportS() {
   var story = "";
   for (var c in Novel.chapters) {
-    story+="Chapter "+(parseInt(c)+1)+"\n"+Novel.chapters[c].text+"\n \n";
+    story+="<h2>"+Novel.chapters[c].title+"</h2><br>"+Novel.chapters[c].text+"<br><br>";
   }
-  alert(story);
+  //document.body.innerHTML += ""+story+"</div>";
+  $("#output").html("<h2 class='text-center' onclick='removeOutput()' style='color: #f22; cursor: pointer'>X</h2><br><br>"+story);
+  $("#output").show();
+  document.getElementById("edit").addEventListener("keyup", function () {
+    if (Novel.chapters[currChap]) {Novel.chapters[currChap].text = document.getElementById("edit").value;}
+  });
+
 }
 
 function updateChapSel() {
@@ -61,9 +64,30 @@ function updateChapSel() {
       document.getElementById('select').innerHTML += " , "
     }
   }
-  document.getElementById('select').innerHTML += "<br><h2><a href='#' onclick='addChap()'>+</a> , <a href='#' onclick='exportS()'>export</a></h2>"
+  document.getElementById('select').innerHTML += "";
+}
+
+function toggleCCN() {
+  $("#changechaptern").toggle(400);
+  document.getElementById("ccnmessage").innerText = "";
+}
+function closeCCN() {
+  $("#changechaptern").hide(400);
+  document.getElementById("ccnmessage").innerText = "";
+}
+function CCN() {
+  try {
+    document.getElementById("ccnmessage").innerText = "";
+    Novel.chapters[parseInt(document.getElementById("ccncn").value)-1].title = document.getElementById("ccncn").value+": "+document.getElementById("ccnnn").value;
+  } catch (e) {
+    document.getElementById("ccnmessage").innerText = "enter a valid chapter number";
+  }
+  document.getElementById("ccncn").value = "";
+  document.getElementById("ccnnn").value = "";
+  updateChapSel();
 }
 
 document.getElementById("edit").addEventListener("keyup", function () {
   if (Novel.chapters[currChap]) {Novel.chapters[currChap].text = document.getElementById("edit").value;}
 });
+addChap();
