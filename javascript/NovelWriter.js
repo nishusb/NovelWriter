d3dbe75241c65e0928799a186ef4b77bf8e33da3
vnms <viniciusbyrne@gmail.com>
2016-11-09 22:33:07 -1000

    character work, settings and more code cleaning

--- javascript/NovelWriter.js
+++ javascript/NovelWriter.js
@@ -13,6 +13,10 @@
   Editor.scen = 0;
   Editor.chap = 0;
   Editor.char = 0;
+  Editor.chapSelText = "";
+  Editor.message = function(c) {
+    $("#message").html(c);
+  }
 }
 
 //Saving and loading
@@ -21,10 +25,10 @@
     var blob = new Blob([JSON.stringify(Novel)], {type: "text/plain;charset=utf-8"});
     saveAs(blob, Novel.title+".novelwriter.txt");
   } function saveN() {
-    messageB("<div class='container'><h1>save story</h1><div class='ob'>"+JSON.stringify(Novel)+"</div>Copy then paste (or <a onclick='saveCodeToTxt()'>save</a>) that code in a text file then load it in when you come back to the site</div>");
+    Editor.message("<h1>save story</h1>Copy then paste (or <a onclick='saveCodeToTxt()'>save</a>) this code in a text file then load it in when you come back to the site<div class='ob'>"+JSON.stringify(Novel)+"</div>");
   } function uploadN() {
-    messageB("<div class='container'><h1>load story</h1>"+
-    "<input id='text' style='outline: 0; min-width: 500; min-height: 200' class='ob' placeholder=''></input><a href='#' class='ob' id='upNov'>load</a></div>");
+    Editor.message("<h1>load story</h1>"+
+    "<input id='text' style='outline: 0; min-width: 500; min-height: 200' class='ob' placeholder=''></input><a href='#' class='ob' id='upNov'>load</a>");
     var tempun = document.getElementById("upNov");
     tempun.addEventListener("mousedown",function() {
       Novel = JSON.parse(text.value);
@@ -41,55 +45,38 @@
   }
 }
 
-//I need to remove this (old variable stuff)
+//Generation/Refreshing of certain important variables
 {
-  var currChap = 0;
-  var currScen = 0;
-  var currChar = 0;
-  var chapMessThing = "";
-}
-
-//Message board (old and needs changes)
-{
-  function messageB(content) {
-    $("#message").show();
-    $("#message").html(content);
-  }
-  function rmMessageB() {
-    $("#message").hide();
-  }
-}
-
-//Refreshing of certain important variables
-{
-  function scbuild() {
-    if (Novel.chapters[currChap]) {
+  function scbuild(edit) {
+    if (edit) {
+      Novel.chapters[Editor.chap].scenes[Editor.scen].text = $("#edit").html();
+    }
+    if (Novel.chapters[Editor.chap]) {
       var scb = "";
-      for (var s in Novel.chapters[currChap].scenes) {
-        scb += Novel.chapters[currChap].scenes[s].text+"<br>";
+      for (var s in Novel.chapters[Editor.chap].scenes) {
+        scb += Novel.chapters[Editor.chap].scenes[s].text+"<br>";
       }
-      Novel.chapters[currChap].text = scb;
+      Novel.chapters[Editor.chap].text = scb;
     }
   }
 
   function exportS() {
     var story = "";
     for (var c in Novel.chapters) {
-      story+="<h2>chapter "+Novel.chapters[c].title+"</h2><div style='font-size: 18px'>"+Novel.chapters[c].text+"</div><br><br>";
+      story+="<h2>Chapter "+Novel.chapters[c].title+"</h2><div style='font-size: 18px'>"+Novel.chapters[c].text+"</div><br><br>";
     }
     return story;
   }
 
   function updateChapSel() {
-    var thing = "<div class='container'><h1>chapters <a href='#' class='ob' style='' onclick='addChap()'>+</a></h1><h2 style='display: inline-block'></h2> ";
+    var thing = "<h1>chapters <a href='#' class='ob' style='' onclick='addChap()'>+</a></h1><h2 style='display: inline-block'></h2> ";
     for (var c in Novel.chapters) {
-      thing += "<h2 style='display: inline'><a href='#' onclick='cc("+(c)+")'>"+"chapter "+Novel.chapters[c].title+"</a></h2>";
+      thing += "<h2 style='display: inline'><a href='#' onclick='cc("+(c)+")'>"+"Chapter "+Novel.chapters[c].title+"</a></h2>";
       if (c < Novel.chapters.length-1) {
         thing += " , "
       }
     }
-    thing += "</div>";
-    chapMessThing = thing;
+    Editor.chapSelText = thing;
   }
 }
 
@@ -111,20 +98,20 @@
     var v = {};
     v.title = "Scene "+t;
     v.text = "";
-    Novel.chapters[currChap].scenes.push(v);
+    Novel.chapters[Editor.chap].scenes.push(v);
   } function createScen() {
-    scene(Novel.chapters[currChap].scenes.length+1);
-    cc(currChap);
+    scene(Novel.chapters[Editor.chap].scenes.length+1);
+    cc(Editor.chap);
   }
 
   function delScene() {
-    Novel.chapters[currChap].scenes.splice(currScen, 1);
+    Novel.chapters[Editor.chap].scenes.splice(Editor.scen, 1);
     scbuild();
-    cc(currChap);
+    cc(Editor.chap);
   }
 
   function delChap() {
-    Novel.chapters.splice(currChap, 1);
+    Novel.chapters.splice(Editor.chap, 1);
     for (var ch in Novel.chapters) {
       Novel.chapters[ch].title = parseInt(ch)+1;
     }
@@ -133,38 +120,56 @@
   }
 
   function changeScenName() {
-    Novel.chapters[currChap].scenes[currScen].title = document.getElementById("csn").value;
-    cs(currScen);
+    Novel.chapters[Editor.chap].scenes[Editor.scen].title = document.getElementById("csn").value;
+    cs(Editor.scen);
   }
 }
 
 //Pages
 {
   function cc(c) {
-    currChap = c;
-    //document.getElementById("edit").innerHTML = Novel.chapters[c].text;
+    Editor.chap = c;
     var scenestext = "Scenes: ";
     for (var s in Novel.chapters[c].scenes) {
       scenestext += "<a href='#' onclick='cs("+s+")'>"+Novel.chapters[c].scenes[s].title+"</a> , ";
     }
     scenestext += "<a href='#' onclick='createScen()'>+</a>";
-    messageB("<div class='container'><h1>chapter "+(c+1)+"</h1><div style='font-size: 18px'>"+Novel.chapters[c].text+"</div><h3>"+
-    scenestext+"</h3>"+"<h2><a class='ob' onclick='delChap()'>delete</a></h2></div>");
+    Editor.message("<h1>Chapter "+(c+1)+"<a class='ob' onclick='delChap()'>delete</a></h1><h3>"+
+    scenestext+"</h3>"+"<div style='font-size: 18px'>"+
+    Novel.chapters[c].text+"</div>");
+  }
+  function cs(s) {
+    Editor.scen = s;
+    Editor.message("<h1>"+Novel.chapters[Editor.chap].scenes[Editor.scen].title+
+    " <p style='display:inline'><input id='csn' class='coolInput' placeholder='change'></input></p> <a href='#' class='ob' onclick='delScene()'>delete</a></h1><h2>"+
+    "</h2><h2><a onclick='cc("+Editor.chap+")' href='#'>chapter "+(Editor.chap+1)+"</a></h2>"+
+    "<div id='edit' contenteditable style='font-size: 20px'></div>"+
+    "<h2></h2>");
+    document.getElementById("edit").innerHTML = Novel.chapters[Editor.chap].scenes[Editor.scen].text;
+    document.getElementById("edit").addEventListener("keyup", function(){scbuild(true)});
+    document.getElementById("csn").addEventListener("keydown", function(e) {
+      if (e.keyCode == 13) {
+        changeScenName();
+      }
+    });
   }
 
   function cchar(c) {
-    currChar = c;
-    messageB("<div class='container'>"+
-      "<h1>"+Novel.characters[currChar].name+"</h1>"+
-      "<h2><a href='#' class='ob' onclick='deleteChar("+c+")'>delete</a></h2></div>");
+    Editor.char = c;
+    var tiv = "Unimportant";
+    Editor.message("<h3 class='ob' onclick='showCharacterPage()'>all characters</h3>"+
+      "<h1>"+Novel.characters[Editor.char].name+"<a href='#' class='ob' onclick='deleteChar("+c+")'>delete</a></h1>"+
+      "<h3>description:<h3><div id='edit' contenteditable style='font-size: 20px'></div>");
+    $("#edit").html(Novel.characters[Editor.char].desc);
+    document.getElementById("edit").onkeyup = function() {
+      Novel.characters[Editor.char].desc = $("#edit").html();
+    }
   }
 
   function showMainPage() {
-    messageB("<div class='container'>"+
-      "<h1>"+Novel.title+" <input id='changeTitle' style='display: inline' placeholder='change'></input></h1>"+
+    Editor.message("<h1>"+Novel.title+" <input id='changeTitle' style='display: inline' placeholder='change'></input></h1>"+
       "<h2>by "+Novel.author+" <input id='changeAuthor' style='display: inline' placeholder='change'></input></h2>"+
-      "<h2><a class='ob' onclick='saveStoryAsHTML()'>save to file</a></h2><div>"+exportS()+"</div>"+
-      "</div>");
+      "<h2><a class='ob' onclick='saveStoryAsHTML()'>save to file</a></h2><div style='font-family: sans-serif'>"+exportS()+"</div>");
       document.getElementById("changeTitle").addEventListener("keydown", function(e) {
         if (e.keyCode==13) {
           Novel.title = document.getElementById("changeTitle").value;
@@ -182,12 +187,15 @@
   function showCharacterPage() {
     var chr = "<h2>";
     for (var cc in Novel.characters) {
-      chr += "<a href='#' class='ob' onclick='cchar("+cc+")'>"+Novel.characters[cc].name+"</a>";
+      if (Novel.characters[Editor.char].important == false) {
+        chr += "<a href='#' class='ob' onclick='cchar("+cc+")'>"+Novel.characters[cc].name+"</a>";
+      } else {
+        chr += "<a href='#' class='ob' onclick='cchar("+cc+")' style='color: #286'>"+Novel.characters[cc].name+"</a>";
+      }
     }
     chr += "</h2>";
-    messageB("<div class='container textcenter'>"+
-      "<h1>characters "+
-      "<input id='addCharacter' placeholder='add'></input>"+chr+"</h1></div>");
+    Editor.message("<h1>characters "+
+      "<input id='addCharacter' placeholder='add'></input>"+chr+"</h1>");
     document.getElementById("addCharacter").addEventListener("keydown", function(e) {
       if (e.keyCode == 13) {
         addCharacter(document.getElementById("addCharacter").value);
@@ -198,23 +206,32 @@
   }
 
   function showChapSel() {
-    messageB(chapMessThing);
+    Editor.message(Editor.chapSelText);
+  }
+}
+//Editor Pages
+{
+  Editor.settings = function() {
+    Editor.message("<h1>settings</h1>"+
+      "<h3>theme: <a href='#' class='ob'>dark</a><a class='ob' href='#'>coming soon</a></h3><h4>More is coming later, hang tight :]</h4>"+
+      "<h3>about:</h3><h4>NovelWriter v0.1 by vnms</h4>");
   }
 }
 
 //Characters
 {
   function addCharacter(n) {
-    Novel.characters.push({name: n, desc: ""});
+    Novel.characters.push({name: n, desc: "", important: false});
   } function deleteChar(c) {
     Novel.characters.splice(c, 1);
     showCharacterPage();
   }
 }
 
-//Don't write code like this at home, kids :|
-//Seriously dont its for your own good
+/* Don't write code like this at home, kids :|
 
-messageB("<div class='container'><h1>NovelWriter</h1>"
+Seriously dont its for your own good */
+
+Editor.message("<h1>NovelWriter</h1>"
   +"<h2>An early-stage writing app by <a href='https://github.com/vnms'>vnms</a></h2>"
-  +"<h2>Hover over the gray bar at the top for menu options</h2><p>Press enter to activate a text input when there is no button</div>");
+  +"<h2>Hover over the gray bar at the top for menu options</h2><h3>Press enter to activate a text input when there is no button</h3>");
